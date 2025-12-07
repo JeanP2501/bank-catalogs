e7c80fe99a020bc2f7a69eb7a3b804520f0e0bed
JeanP2501 <jeanydrogos@gmail.com>
2025-12-06 19:18:27 -0500

    first commit

---
+++ src/test/java/com/bank/catalogs/CatalogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.catalogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
